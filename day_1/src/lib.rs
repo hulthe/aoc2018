77c9ce5d320951e1c451fdaa95187177440a63c6
Joakim Hulthe <joakim@hulthe.net>
2018-12-01 17:11:05 +0100

    Use B-Trees for day 1 dup task
    Reduce execution time by ~95% for 10000 iterations

--- day_1/src/lib.rs
+++ day_1/src/lib.rs
@@ -1,38 +1,44 @@
-use std::io::{BufRead};
+#![feature(test)]
+use std::collections::BTreeSet;
 use std::error::Error;
+use std::io::BufRead;
 
-fn parse_freqs<'a, R: BufRead + 'a>(reader: R) -> Box<(dyn Iterator<Item=i32> + 'a)> {
-    let iter = reader.lines()
+extern crate test;
+
+fn parse_freqs<'a, R: BufRead + 'a>(reader: R) -> Box<(dyn Iterator<Item = i32> + 'a)> {
+    let iter = reader
+        .lines()
         .filter_map(|l| l.ok())
         .filter_map(|s| s.parse::<i32>().ok());
     Box::new(iter)
 }
 
 pub fn sum_freqs<R: BufRead>(reader: R) -> Result<i32, Box<Error>> {
-    let sum = parse_freqs(reader)
-        .sum();
+    let sum = parse_freqs(reader).sum();
 
     Ok(sum)
 }
 
 pub fn freqs_first_dup<R: BufRead>(reader: R) -> Result<i32, Box<Error>> {
     let pattern: Vec<i32> = parse_freqs(reader).collect();
-    let mut history: Vec<i32> = vec![0];
+    let mut history: BTreeSet<i32> = BTreeSet::new();
+
+    let mut last = 0;
     loop {
         for num in pattern.iter() {
-            let new = history.last().unwrap() + num;
-            if history.contains(&new) {
-                return Ok(new);
+            if !history.insert(last) {
+                return Ok(last);
             }
-            history.push(new);
+            last += num;
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::io;
+    use self::test::Bencher;
     use super::*;
+    use std::io;
 
     #[test]
     fn sum_test() {
@@ -47,11 +53,22 @@ mod tests {
             ("+3\n+3\n+4\n-2\n-4", 10),
             ("-6\n+3\n+8\n+5\n-6", 5),
             ("+7\n+7\n-2\n-7\n-4", 14),
+            ("+9999\n+1\n-9999\n+1", 9999),
         ];
         let cases = cr.iter_mut().map(|(i, r)| (io::Cursor::new(i), r));
 
         for (i, r) in cases {
             assert_eq!(freqs_first_dup(i).unwrap(), r.clone());
         }
+    }
+
+    #[bench]
+    fn bench_sum_range(b: &mut Bencher) {
+        b.iter(sum_test)
+    }
+
+    #[bench]
+    fn bench_find_dup(b: &mut Bencher) {
+        b.iter(first_dup_test)
     }
 }
